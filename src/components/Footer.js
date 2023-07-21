bc0844b8fc69e736421ec504ca4694cb8df66ee1
AmanRaj08 <amanraj08072001@gmail.com>
2023-07-21 23:42:16 +0530

    first

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <section class="footer">
+        <div class="links">
+          <a href="#">home</a>
+          <a href="#">about</a>
+          <a href="#">menu</a>
+          <a href="#">products</a>
+          <a href="#">review</a>
+          <a href="#">contact</a>
+          <a href="#">blogs</a>
+        </div>
+        <div class="credit">
+          created by <span>Aman Raj</span> | all rights reserved
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/components/Review.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { review } from "../Data";
+import qouteImg from "../assets/images/quote-img.png";
+const Review = () => {
+  return (
+    <>
+      <section className="review" id="review">
+        <h1 className="heading">
+          customer's <span>review</span>
+        </h1>
+
+        <div className="box-container">
+          {review.map((item, index) => (
+            <div className="box" key={index * Math.random()}>
+              <img src={qouteImg} alt="" className="quote" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+                nulla sit libero nemo fuga sequi nobis? Necessitatibus aut
+                laborum, nisi quas eaque laudantium consequuntur iste ex aliquam
+                minus vel? Nemo.
+              </p>
+              <img src={item.img} className="user" alt="" />
+              <h3>Santosh kumar</h3>
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Review;
